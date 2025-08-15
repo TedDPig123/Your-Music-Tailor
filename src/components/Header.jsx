faa1ccb03be77e0823a7200660223abfc8c54c52
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2025-08-15 13:05:41 -0400

    spacing mobile fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <div id="header" className="text-[50px] font-helveticaregular tracking-[20px] w-full flex justify-center mt-15">
             <div className='flex gap-4 items-center'>
-                <img src={bird} className='w-[70px] rounded-full' alt="logo" />
+                <img src={bird} className='w-[60px] rounded-full' alt="logo" />
                 <div>SONGBIRD</div>
             </div>
         </div>
